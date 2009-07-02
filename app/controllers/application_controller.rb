72eb75fe4f6b79b5e2887fe59b6cd1d2952208eb
Alexey Noskov <alexey.noskov@gmail.com>
2009-07-02 15:57:07 +0400

    Login by params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   
   include ExceptionNotifiable
+  include SessionHandling
     
   layout 'default'
   
@@ -8,42 +9,5 @@ class ApplicationController < ActionController::Base
   
   helper :all # include all helpers, all the time
   helper_method :current_user_session, :current_user
-  
-  protected
-  
-  def current_user_session
-    @current_user_session ||= UserSession.find
-  end
-
-  def current_user
-    @current_user ||= current_user_session && current_user_session.user
-  end
-  
-  def require_user
-    unless current_user
-      store_location
-      flash[:error] = I18n.t('error.login_required')
-      redirect_to root_url
-      return false
-    end
-  end
-  
-  def load_user_using_perishable_token
-    @user = User.find_using_perishable_token(params[:id])
-    
-    unless @user
-      flash[:error] = I18n.t('error.no_user_found')
-      redirect_to root_url
-    end
-  end
-
-  def store_location
-    session[:return_to] = request.request_uri
-  end
-
-  def redirect_back_or_default(default)
-    redirect_to(session[:return_to] || default)
-    session[:return_to] = nil
-  end
 
 end
---
+++ lib/session_handling.rb
@@ -0,0 +1,45 @@
+module SessionHandling
+   
+  def current_user_session
+    return @current_user_session if @current_user_session # Если сесссия уже есть - выход
+    
+    if params[:email] && params[:password] # Автоматический логин по параметрам
+      @current_user_session = UserSession.new( :email => params[:email], :password => params[:password], :remember_me => params[:remember_me] )
+      @current_user_session = nil unless @current_user_session.save
+    end
+    
+    @current_user_session = UserSession.find # Восстановление сессии
+  end
+
+  def current_user
+    @current_user ||= current_user_session && current_user_session.user
+  end
+  
+  def require_user
+    unless current_user
+      store_location
+      flash[:error] = I18n.t('error.login_required')
+      redirect_to root_url
+      return false
+    end
+  end
+  
+  def load_user_using_perishable_token
+    @user = User.find_using_perishable_token(params[:id])
+    
+    unless @user
+      flash[:error] = I18n.t('error.no_user_found')
+      redirect_to root_url
+    end
+  end
+
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
+  end
+  
+end
