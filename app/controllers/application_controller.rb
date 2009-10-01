bf54d44077d2cfe22250c6d412fdf32b417cde09
Alexey Noskov <alexey.noskov@gmail.com>
2009-10-02 01:14:39 +0400

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   
   filter_parameter_logging :password, :password_confirmation
   
-  helper :all # include all helpers, all the time
   helper_method :current_user_session, :current_user
 
 end
