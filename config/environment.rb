08884939c4d1e4f5f7baf9ef62eec4e846087161
Alexey Noskov <alexey.noskov@gmail.com>
2009-10-27 14:57:07 +0300

    Using irwi from gem, some fixes in styles

--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "spreadsheet" 
+  config.gem "irwi"
   
   config.gem "mislav-will_paginate", :lib => 'will_paginate', :source => "http://gems.github.com"
   
