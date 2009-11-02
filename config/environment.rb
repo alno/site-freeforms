77d09b0e13e33def59d3dab56814ad056c72b6b0
Alexey Noskov <alexey.noskov@gmail.com>
2009-11-02 14:41:11 +0300

    Front page, styling

--- config/environment.rb
+++ config/environment.rb
@@ -22,9 +22,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "spreadsheet" 
-  config.gem "irwi"
-  
-  config.gem "mislav-will_paginate", :lib => 'will_paginate', :source => "http://gems.github.com"
+  config.gem "irwi"  
+  config.gem "will_paginate"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
