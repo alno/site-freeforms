0b266cf6410d38112e21e8834f416432e4cb0cc5
Alexey Noskov <alexey.noskov@gmail.com>
2009-12-22 20:47:59 +0300

    Service stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.wiki_root '/pages'
-  
+
   map.resources :forms, :member => [ :code, :clone, :messages, :unread, :today ]  
   map.resources :messages, :collection => [ :unread, :today ]
-    
+
   map.resource :account
   map.resource :session
-  
+
   map.resources :account_passwords
   map.resources :account_activations  
-  
+
+  map.resources :stats
+
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   
   map.post_message '/post/:form_id', :controller => 'root', :action => 'post'
   map.message_status '/status/:token', :controller => 'root', :action => 'status'
-    
+  
   map.root :controller => 'root', :action => 'index'  
   map.register '/register', :controller => 'root', :action => 'register'
   map.restore '/restore', :controller => 'root', :action => 'restore'
