60938dc00872d0b1791906079240cd99e516da00
Alexey Noskov <alexey.noskov@gmail.com>
2010-02-03 11:50:08 +0300

    Delayed job

--- config/environment.rb
+++ config/environment.rb
@@ -16,6 +16,7 @@ Rails::Initializer.run do |config|
   config.gem "spreadsheet" 
   config.gem "irwi"  
   config.gem "will_paginate"
+  config.gem "delayed_job"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
