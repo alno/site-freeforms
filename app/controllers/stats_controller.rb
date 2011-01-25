df3da20a5419a31bceb5fbd8b293e0f10136eb26
Alexey Noskov <alexey.noskov@gmail.com>
2011-01-25 23:15:37 +0300

    Some beauty

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -11,8 +11,9 @@ class StatsController < ApplicationController
   def count_chart_for( model )
     SmartChart::Line.new :width => 400, :height => 200, 
       :y_min => 0, :y_max => 1000,
-      :data => (-39..0).map {|v| model.where( "#{model.table_name}.created_at < ?", Time.now + v.weeks ).count },
-      :grid => { :x => { :every => 5 }, :y => { :every => 100 }, :style => :dashed }
+      :data => (-11..0).map {|v| model.where( "#{model.table_name}.created_at < ?", Time.now + v.months ).count },
+      :grid => { :x => { :every => 3 }, :y => { :every => 100 }, :style => :dashed },
+      :axis => { :left => { :color => 'DDDDDD', :ticks => 'DDDDDD', :labels => { 0 => 0, 500 => 500, 1000 => 1000 } } }
   end
 
 end
