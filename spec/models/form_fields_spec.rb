9a807e452abba9625da50e0ed1204eef1b20d0b2
Alexey Noskov <alexey.noskov@gmail.com>
2013-05-22 00:10:20 +0400

    Simplify spec_helper require

--- spec/models/form_fields_spec.rb
+++ spec/models/form_fields_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
 RSpec::Matchers.define :accept do |value|
   match { |given| given.error_for(value).nil? }
